f587352dd2ecdff96441dd822e0b3b92adf78aff
Mickael MAGNIEZ <mickael.magniez@rueducommerce.fr>
2017-10-25 17:08:00 +0200

    hello bootstrap

--- app/controllers/ParseController.scala
+++ app/controllers/ParseController.scala
@@ -10,40 +10,89 @@ import org.stringtemplate.v4._
 import java.io.FileInputStream
 import java.nio.charset.StandardCharsets
 
-import grammars.tsql.{TSqlFileVisitor, TSqlLexer, TSqlSelectListVisitor}
+import grammars.tsql.{TSqlFileVisitor, TSqlLexer, TSqlParser, TSqlSelectListVisitor}
 import org.antlr.runtime.ANTLRStringStream
+import play.api.data._
+import play.api.data.Forms._
 
 import scala.collection.JavaConverters._
+
+
+
+case class sqlFormData(sql: String)
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class ParseController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ParseController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
+  def getParser(statements: String): TSqlParser = {
+    val statementsUpper = statements.toUpperCase
+    val stream = new ByteArrayInputStream(statementsUpper.getBytes(StandardCharsets.UTF_8))
+    val lexer = new TSqlLexer(CharStreams.fromStream(stream, StandardCharsets.UTF_8))
+    val tokens = new CommonTokenStream(lexer)
+    new grammars.tsql.TSqlParser(tokens)
+  }
   /**
    * Create an Action to render an HTML page with a welcome message.
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
-    val lines = scala.io.Source.fromFile("/data/perso/dev/rme-web/public/data/sample.sql").mkString
-//    val input = new ANTLRStringStream(lines)
-    val stream = new ByteArrayInputStream(lines.getBytes(StandardCharsets.UTF_8))
-    val lexer = new TSqlLexer(CharStreams.fromStream(stream, StandardCharsets.UTF_8))
-    val tokens = new CommonTokenStream(lexer)
-    val parser = new grammars.tsql.TSqlParser(tokens)
-    val prog = parser.tsql_file()
+  def index = Action { implicit request =>
+//    val lines = scala.io.Source.fromFile("/home/mickael/work/vp/rme-web/public/data/sample.sql").mkString
+////    val input = new ANTLRStringStream(lines)
+//    val stream = new ByteArrayInputStream(lines.getBytes(StandardCharsets.UTF_8))
+//    val lexer = new TSqlLexer(CharStreams.fromStream(stream, StandardCharsets.UTF_8))
+//    val tokens = new CommonTokenStream(lexer)
+//    val parser = new grammars.tsql.TSqlParser(tokens)
+//    val prog = parser.tsql_file()
+//
+//    println (lines)
+//    println (prog.toString)
+//
+//    val vis = new TSqlFileVisitor("file")
+//    println ("ici")
+//
+//    var schema = vis.getSchema(parser)
+//    println ("la")
+//    println (schema.tables.length)
+//    println(schema.marshallJson())
+//
+//    println("=====")
+    var sqlForm = Form(
+      mapping(
+        "sql" -> text
+      )(sqlFormData.apply)(sqlFormData.unapply)
+    )
+    sqlForm.bindFromRequest.fold(
+      formWithErrors => {
+        println("error")
+        println(formWithErrors.toString())
+        BadRequest(views.html.parse(formWithErrors, 0, ""))
+      },
+      sql => {
+        println("ok")
+        println(sql.sql)
+        sqlForm = sqlForm.fill(sql)
+        val parser = getParser(sql.sql)
 
-    println (prog.toString)
+        val vis = new TSqlFileVisitor("file")
+        val schema = vis.getSchema(parser)
 
-    val vis = new TSqlFileVisitor("file")
-    println ("ici")
-    var schema = vis.getSchema(parser)
-    println ("la")
+        println(schema.marshallJson())
+//        val contactId = Contact.save(contact)
+//        Redirect(routes.Application.showContact(contactId)).flashing("success" -> "Contact saved!")
 
-    Ok(views.html.parse("Parse"))
+        Ok(views.html.parse(sqlForm, schema.tables.length, schema.marshallJson()))
+      }
+    )
+
+//    Redirect(controllers.routes.ParseController.index())
+
+
+//    Ok(views.html.parse(sqlForm, schema.tables.length))
   }
 }
--- build.sbt
+++ build.sbt
@@ -11,3 +11,8 @@ scalaVersion := "2.12.2"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 libraryDependencies += "com.h2database" % "h2" % "1.4.196"
+libraryDependencies += filters
+libraryDependencies += "org.webjars" % "jquery" % "3.2.1"
+libraryDependencies += "org.webjars" % "bootstrap" % "4.0.0-beta-1"
+libraryDependencies += "com.adrianhurt" %% "play-bootstrap" % "1.2-P26-B4"
+
